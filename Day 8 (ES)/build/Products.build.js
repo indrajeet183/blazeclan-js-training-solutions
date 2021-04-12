46d7ede7e98614843e8d81cb814abd66e50fe378
Indrajeet Latthe <indrajeet.latthe@weverve.com>
2021-04-12 17:35:31 +0530

    day 8 - excercise solution

---
+++ Day 8 (ES)/build/Products.build.js
@@ -0,0 +1,22 @@
+const getTable = products => {
+  //console.log(products)
+  let table = "<table><thead>";
+
+  if (products.length) {
+    const header = getHeaders(Object.keys(products[0]));
+    table += `${header}</thead><tbody>`;
+    table += products.map(product => {
+      let tds = Object.keys(product).map(key => {
+        //console.log(`<td>${product[key]}</td>`)
+        return `<td>${product[key]}</td>`;
+      }).join(''); //console.log(`<tr>${tds}</tr>`)
+
+      return `<tr>${tds}</tr>`;
+    }).join('');
+  } else {
+    table = 'No products data!';
+  }
+
+  table += "</tbody></table>";
+  return table;
+};
---
+++ Day 8 (ES)/src/Headers.js
@@ -0,0 +1,13 @@
+const getHeaders = (headers) => {
+    //console.log(products)
+    let headerResult = ""
+    if (headers.length) {
+        headerResult = headers.map((header) => {
+            return `<th>${header}</th>`
+        }).join("")
+        headerResult = `<tr>${headerResult}</tr>`
+    } else {
+        headerResult = 'No headers data!'
+    }
+    return headerResult
+}
